253ea8790820867917364f0f9f6767499e566b62
Kirill Vetchinkin <kirill.vetchinkin@instamart.ru>
2021-08-21 20:15:00 +0300

    fix

--- magazine.go
+++ magazine.go
@@ -1,16 +1,17 @@
 package magazine
 
-type Subscriber struct {
-	Name   string
-	Rate   float64
-	Active bool
-}
-
 type Address struct {
 	Street     string
 	City       string
 	State      string
 	PostalCode string
+}
+
+type Subscriber struct {
+	Name   string
+	Rate   float64
+	Active bool
+	Address
 }
 
 type Employee struct {
