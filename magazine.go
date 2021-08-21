6ff3e5a9381a8e2ce20a91f4aad88fe523119baa
Kirill Vetchinkin <kirill.vetchinkin@instamart.ru>
2021-08-21 19:47:12 +0300

    fix

--- magazine.go
+++ magazine.go
@@ -4,3 +4,14 @@ type Car struct {
 	Name  string
 	Speed int
 }
+
+type Subscriber struct {
+	Name   string
+	Rate   float64
+	Active bool
+}
+
+type Employee struct {
+	Name   string
+	Salary float64
+}
